d7e6ca413dfdede07d2a9fb483b9f1a8003d50fd
muratayusuke <info@muratayusuke.com>
2013-06-28 15:12:07 +0900

    revert bindkey to default

--- zaw.zsh
+++ zaw.zsh
@@ -216,11 +216,6 @@ function select-action() {}; zle -N select-action
 filter-select -i
 bindkey -M filterselect '^i' select-action
 
-bindkey '^gz' zaw
-bindkey '^g^b' zaw-git-recent-all-branches
-bindkey '^g^h' zaw-history
-bindkey '^gc' zaw-cdr
-bindkey '^ga' zaw-ack
-bindkey '^gs' zaw-git-status
+bindkey '^X;' zaw
 
 }
